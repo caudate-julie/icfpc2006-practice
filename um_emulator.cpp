dd2afad69e091107e524f7dcf12c35486e94a40c
caudate_julie <kyra-mensk@yandex.ru>
2018-07-19 00:06:59 +0300

    Added (not supported) modes and early failures. UM runs and passes SANDmark (but slow)

--- um_emulator.cpp
+++ um_emulator.cpp
@@ -3,9 +3,12 @@
 #include <pybind11/stl.h>
 #include <pybind11/pybind11.h>
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <string>
 #include <sstream>
+#include <cassert>
+//#include <algorithm>
 
 using std::vector;
 using std::string;
@@ -30,11 +33,18 @@ public:
 	uint32 exec_finger;
 	uint32 regs[8];
 	bool halt;
+	bool waiting_for_input;
 
 	std::stringstream in;
 	std::stringstream out;
 	string error_message;
 
+    enum MODE { 
+                debug = 1,
+                echo = 2,
+                echoinput = 4,
+    };
+    unsigned int mode;
 
 	/**-----------------------------------------------------
 	  Empty universal machine. 
@@ -45,7 +55,7 @@ public:
 		for (uint32& x : regs) x = 0;
 		arrays.push_back(vector<uint32>());
 		exec_finger = 0;
-		halt = false;
+		halt = waiting_for_input = false;
 		error_message = "";
 	}
 
@@ -79,12 +89,25 @@ public:
 	}
 
 	void _1_array_index(uint32 p) {
-		// TODO: abandonment failure
+		for (uint32 x : abandoned) {
+			if (x == B(p)) return fail_operation("Calling abandoned array");
+		}
+		if (arrays.size() <= B(p) || arrays[B(p)].size() <= C(p)) {
+			return fail_operation("Index out of bounds");
+		}
 		A(p) = arrays[B(p)][C(p)];
 	}
 
 	void _2_array_amendment(uint32 p) {
-		// TODO: abandonment failure
+		for (uint32 x : abandoned) {
+			if (x == A(p)) {
+				fail_operation("Calling abandoned array");
+				return;
+			}
+		}
+		if (A(p) >= arrays.size() || B(p) >= arrays[A(p)].size()) {
+			return fail_operation("Index out of bounds");
+		}
 		arrays[A(p)][B(p)] = C(p);
 	}
 
@@ -97,7 +120,7 @@ public:
 	}
 	
 	void _5_division(uint32 p) {
-		// TODO: zero division fail
+		if (!C(p)) return fail_operation("Zero division");
 		A(p) = B(p) / C(p);	
 	}
 
@@ -110,53 +133,69 @@ public:
 	}
 
 	void _8_allocation(uint32 p) {
-		if (!abandoned.empty()) {
-			B(p) = abandoned.back();
-			abandoned.pop_back();
-			arrays[B(p)] = vector<uint32>(C(p));
+		// don't use B(p) instead of index! B(p) and C(p) may be same register.
+		if (abandoned.empty()) {
+			uint32 index = arrays.size();
+			arrays.push_back(vector<uint32>(C(p)));
+			B(p) = index;
 		}
 		else {
-			B(p) = arrays.size();
-			arrays.push_back(vector<uint32>(C(p)));
+			uint32 index = abandoned.back();
+			abandoned.pop_back();
+			arrays[index] = vector<uint32>(C(p));
+			B(p) = index;
 		}
 	}
 
 	void _9_abandonment(uint32 p) {
-		// ?TODO: abandonment failure
+		if (C(p) == 0) return fail_operation("Abandoning working program");
 		abandoned.push_back(C(p));
 	}
 
 	void _10_output(uint32 p) {
-		// TODO: >256 failure
-		// TODO: echo mode
+		if (C(p) > 255) return fail_operation("Output of non-ASCII");
 		out << (char)C(p);
+		if (mode & MODE::echo) std::cout << (char)C(p);
 	}
 	
 	void _11_input(uint32 p) {
-		// TODO: EOF
-		char c;
-		in >> c;
+		unsigned char c;
+		if (!(in >> c)) {
+			waiting_for_input = true;
+			exec_finger--;
+			return;
+		}
 		C(p) = (uint32)c;
-		// TODO: copy input mode
+		if (mode & MODE::echoinput) _10_output(p);
 	}
 	
 	void _12_load_program(uint32 p) {
+		for (uint32 x : abandoned) {
+			if (x == B(p)) return fail_operation("Calling abandoned array");
+		}
 		if (B(p)) arrays[0] = arrays[B(p)];
+		if (C(p) >= arrays[0].size()) return fail_operation("Finger out of bounds");
 		exec_finger = C(p);
 	}
 	
 	void _13_orthography(uint32 p) {
-		uint32& A = regs[(p >> 25) & 7];
-		uint32 value = p && ((1 << 25) - 1);
-		A = value;
+		uint32 index = (p >> 25) & 7;
+		uint32 value = p & ((1 << 25) - 1);
+		regs[index] = value;
 	}
 	
 	void _14_15_illegal(uint32 p) {
-		// TODO: fail
+		fail_operation("Illegal operation");
 	}
 
+	void fail_operation(string message) {
+		error_message = message;
+		halt = true;
+	}
+
+
 	typedef void(UMEmulator::*operation)(uint32);
-	operation operationlist[15] = {
+	constexpr static operation operationlist[] = {
 								&UMEmulator::_0_conditional_move,
 								&UMEmulator::_1_array_index,
 								&UMEmulator::_2_array_amendment,
@@ -171,6 +210,7 @@ public:
 								&UMEmulator::_11_input,
 								&UMEmulator::_12_load_program,
 								&UMEmulator::_13_orthography,
+								&UMEmulator::_14_15_illegal,
 								&UMEmulator::_14_15_illegal
 								};
 	
@@ -183,6 +223,8 @@ public:
 	
 	/**-----------------------------------------------------
 	  Load program from given filename.
+	  Returns if the machine is waiting for input (input 
+	  stream is empty).
 	  ----------------------------------------------------*/
 	void load(string file) {
 		std::ifstream s(file, std::ios::binary);
@@ -195,7 +237,7 @@ public:
 		arrays[0].clear();
 		unsigned char byte;
 		uint32 word = 0;
-		int count = 0;
+		unsigned count = 0;
 
 		while (s >> byte) {
 			word = (word << 8) + byte;
@@ -205,7 +247,33 @@ public:
 				word = count = 0;
 			}
 		}
+		assert (count == 0);
 	}
+
+
+	void setmode(vector<MODE> modelist) {
+		mode = 0;
+		for (MODE m : modelist) mode |= m;
+	}
+
+
+	/**-----------------------------------------------------
+	  --------------------  RUUUUUUN!-----------------------
+	  ----------------------------------------------------*/
+	bool run(string s) {
+		in.str(s);
+		while (!halt && !waiting_for_input) {
+			uint32 platter = arrays[0][exec_finger++];
+			uint32 cmd = get_command(platter);
+			std::invoke(operationlist[cmd], this, platter);
+			if (exec_finger >= arrays[0].size()) fail_operation("Finger out of bounds");
+		}
+		assert (halt ^ waiting_for_input);
+		return waiting_for_input;
+	}
+
+
+};
 
 
 
@@ -222,5 +290,15 @@ PYBIND11_MODULE(um_emulator, m) {
 		.def_readonly("error_message", &UMEmulator::error_message)
 
 		.def("load", &UMEmulator::load)
+		.def("setmode", &UMEmulator::setmode)
+		.def("run", &UMEmulator::run)
+
 	;
+
+	py::enum_<UMEmulator::MODE>(UMclass, "mode")
+        .value("debug", UMEmulator::MODE::debug)
+        .value("echo", UMEmulator::MODE::echo)
+        .value("echoinput", UMEmulator::MODE::echoinput)
+        .export_values()
+    ;
 }
