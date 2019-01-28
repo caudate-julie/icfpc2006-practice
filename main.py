54aa1ba747f122c0615466245abb2b664594b5df
caudate_julie <kyra-mensk@yandex.ru>
2019-01-28 22:44:48 +0300

    Game : collect score from out-files

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from clients import *
-import logging
-from time import time
-import pathlib
 from cpp.um_emulator import UniversalMachine
+
+from time import time
+from collections import defaultdict
+import logging
+import pathlib
+import re
 
 
 def run_user():
@@ -15,9 +18,35 @@ def run(clientlist):
     for client in clientlist:
         client.run(um)
 
+
 def collect_score():
-    pass
+    # PUZZL.TSK=100@1001|14370747643c6d2db0a40ecb4b0bb65
+    # | 1 |     |2|
+    # problem   score
+    regex = re.compile(r'(\w*?)\.\w*?=(\d*)@\d*\|\w*')
+    score = 0
+    scorelines = defaultdict(set)
+    for p in Path('logs').glob('*.out'):
+        with p.open() as f:
+            for line in f:
+                m = regex.match(line)
+                if m is None:
+                    continue
+                if line in scorelines[m[1]]:
+                    continue
+                scorelines[m[1]].add(line)
+                score += int(m[2])
+
+    with Path('logs/score.txt').open('w') as f:
+        f.write(f'Total score: {score}\n')
+        for task in scorelines.keys():
+            f.write('\n' + task + '\n')
+            for line in scorelines[task]:
+                f.write(line)
+
+
 
 if __name__ == '__main__':
-    clientlist = [UserClient('EOU', first=True)]
-    run(clientlist)
+    collect_score()
+    # clientlist = [UserClient('EOU', first=True)]
+    # run(clientlist)
